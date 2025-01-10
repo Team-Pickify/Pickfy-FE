19d7bc6cb50380718cb35ddb95a58bdb94aa9860
sungwonnoh <jessica20730@naver.com>
2025-01-10 10:33:46 +0900

    ✨ feat: 페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
+import Main from "./pages/main";
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </>
   );
 }
