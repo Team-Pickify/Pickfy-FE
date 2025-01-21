5c2e1771e518e1cd7db7de24e4f8c4444edddafe
zoo <anfdnfl@khu.ac.kr>
2025-01-21 23:10:50 +0900

    ✨ Feat: 헤더 컴포넌트 완성

---
+++ src/components/header/HeaderBtn.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+// import { IoIosArrowRoundBack } from "react-icons/io";
+import { GoArrowLeft } from "react-icons/go";
+import { CiShare2 } from "react-icons/ci";
+import { LuUserRound } from "react-icons/lu";
+import { CiHeart } from "react-icons/ci";
+import { theme } from "../../styles/themes";
+import { useNavigate } from "react-router-dom";
+
+const Btn = styled.div`
+  cursor: pointer;
+  width: 2rem;
+`;
+
+const icons = {
+  back: { ic: <GoArrowLeft size={28} color={theme.Sub1} />, addr: -1 },
+  share: { ic: <CiShare2 size={28} color={theme.Sub1} />, addr: "" },
+  setting: {
+    ic: <LuUserRound size={28} color={theme.Sub1} />,
+    addr: "/setting",
+  },
+  myplacelist: {
+    ic: <CiHeart size={28} color={theme.Sub1} />,
+    addr: "/myplacelist",
+  },
+};
+
+export default function HeaderBtn({ pageLoc, pos }) {
+  const navigate = useNavigate();
+  const handleNav = (address) => navigate(address);
+
+  let icon;
+  if (pageLoc === "main") {
+    icon = pos === "left" ? icons.setting : icons.myplacelist;
+  } else if (pageLoc === "myplacelist") {
+    icon = pos === "left" ? icons.back : icons.share;
+  } else if (pageLoc === "setting") {
+    icon = pos === "left" ? icons.back : "none";
+  }
+
+  return <Btn onClick={() => handleNav(icon.addr)}>{icon.ic}</Btn>;
+}
--- src/layouts/navbar.jsx
+++ src/layouts/navbar.jsx
@@ -1,18 +1,30 @@
 import styled from "styled-components";
+import logoImg from "../assets/logo.png";
+import HeaderBtn from "../components/header/headerBtn";
 
 const Header = styled.div`
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 1.5rem 0.2rem 1.5rem;
   height: 6.5rem;
-  padding: 3.5rem 1.25rem 1.5rem 1.25rem;
-  flex-shrink: 0;
   background-color: white;
   border-bottom: 1px solid #e6e6e6;
 `;
 
+const Logo = styled.div`
+  background: url(${(props) => props.logo}) no-repeat;
+  background-size: cover;
+  width: 6rem;
+  height: 2rem;
+`;
+
 export default function Navbar() {
   return (
-    <>
-      <Header>헤더바</Header>
-    </>
+    <Header>
+      <HeaderBtn pageLoc="myplacelist" pos="left" />
+      <Logo logo={logoImg} />
+      <HeaderBtn pageLoc="myplacelist" pos="right" />
+    </Header>
   );
 }
