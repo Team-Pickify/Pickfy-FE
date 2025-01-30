48492d9b3127f4c4665938e8222fe48611111e29
zoo <anfdnfl@khu.ac.kr>
2025-01-30 15:25:02 +0900

    ✨ Feat: 토스트 관련 컴포넌트들 폴더 하나에 넣고, 체크 표시 포함된 메세지 박스 컴포넌트 생성(CheckMsg)

---
+++ src/components/toast/Toast.jsx
@@ -0,0 +1,63 @@
+import { useEffect } from "react";
+import styled, { keyframes } from "styled-components";
+
+const fadeIn = keyframes`
+  0%{
+    opacity: 0%;
+  }
+  50%{
+    opacity: 100%;
+  }
+  100%{
+    opacity: 0%;
+  }
+`;
+
+const ToastWrapper = styled.div`
+  width: 100vw;
+
+  @media screen and (min-width: 600px) {
+    width: 600px;
+  }
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: fixed;
+  bottom: 2.5rem;
+
+  animation: ${fadeIn} 3s forwards;
+`;
+
+const ToastContainer = styled.div`
+  width: 88vw;
+
+  @media screen and (min-width: 600px) {
+    width: 550px;
+  }
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem 2rem;
+
+  font-size: 1rem;
+  color: white;
+  background-color: black;
+  border-radius: 2rem;
+`;
+
+export default function Toast({ message, setToastVisible }) {
+  useEffect(() => {
+    setTimeout(() => {
+      setToastVisible(false);
+    }, 3000);
+  }, []);
+
+  return (
+    <ToastWrapper>
+      <ToastContainer>{message}</ToastContainer>
+    </ToastWrapper>
+  );
+}
