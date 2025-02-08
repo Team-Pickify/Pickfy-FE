c5828aeab626418b708fff4c0d84f2bff930fc16
5eungwon <kimseung0630@naver.com>
2025-02-08 18:27:38 +0900

    ✨ 카테고리 목록 조회 컴포넌트

---
+++ src/hooks/getCategorylist.jsx
@@ -0,0 +1,19 @@
+import { TokenReq as getApi } from '../apis/axiosInstance';
+
+const getCategorylist = async(setcategorybtn,setcategoryarray)=>{
+    await getApi.get("/categories")
+    .then((res)=>{
+      console.log(res.data.result)
+      const newarr = res.data.result.map((v)=>{
+         if(v.name === '전체'){
+           return 1;
+        }
+        return 0})
+      console.log(newarr)
+      setcategorybtn(newarr)
+      setcategoryarray(res.data.result)
+    })
+    .catch((error)=>{console.error(error)})
+  }
+
+export default getCategorylist
