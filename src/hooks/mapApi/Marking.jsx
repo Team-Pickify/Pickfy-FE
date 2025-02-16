7e8bf00f17b34e3ef226fd214fd61aa10c5b95d5
5eungwon <kimseung0630@naver.com>
2025-02-16 22:13:53 +0900

    ✨ 변수 일부분 수정

--- src/hooks/mapApi/Marking.jsx
+++ src/hooks/mapApi/Marking.jsx
@@ -1,4 +1,5 @@
 import redMarker from "../../assets/redmarker.svg"
+import blackMarker from "../../assets/black_marker.svg"
 
 const Marking = (datas,setinfoData,curmap,handleOpenBottomSheet,setimagearray)=>{
 
@@ -10,6 +11,7 @@ const Marking = (datas,setinfoData,curmap,handleOpenBottomSheet,setimagearray)=>
     var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
 
     const markers = datas.map((v,i)=>{
+      const iconImg = v.iconUrl
       const marker = new kakao.maps.Marker({
           map: curmap,
           position: new kakao.maps.LatLng(v.latitude,v.longitude),
@@ -19,7 +21,7 @@ const Marking = (datas,setinfoData,curmap,handleOpenBottomSheet,setimagearray)=>
         setimagearray(v.imageUrls)
         setinfoData({
           name:v.name,
-          categoryName:v.categories[0],
+          categoryName:v.categoryName,
           shortDescription : v.shortDescription,
           instagramLink:v.instagramLink,
           naverLink:v.naverLink
@@ -27,7 +29,38 @@ const Marking = (datas,setinfoData,curmap,handleOpenBottomSheet,setimagearray)=>
         handleOpenBottomSheet();
       });
       marker.setMap(curmap)
-      })
+      const overlayDiv = document.createElement("div");
+      overlayDiv.style.position = "absolute";
+      overlayDiv.style.bottom = "0.3rem"; // 마커 빈 부분에 맞추기
+      overlayDiv.style.left = "50%";
+      overlayDiv.style.transform = "translateX(-48%)";
+      overlayDiv.style.width = "3.1rem";  // 고정 크기 유지
+      overlayDiv.style.height = "3.1rem";
+      overlayDiv.style.borderRadius = "50%"; 
+      overlayDiv.style.overflow = "hidden";
+
+    // 🔥 이미지 크기를 부모 크기에 맞게 % 적용
+    overlayDiv.innerHTML = `<img src="${iconImg}" style="width: 100%; height: 100%; border-radius: 50%;" />`;
+
+    overlayDiv.addEventListener("click", () => {
+      setimagearray(v.imageUrls)
+        setinfoData({
+          name:v.name,
+          categoryName:v.categoryName,
+          shortDescription : v.shortDescription,
+          instagramLink:v.instagramLink,
+          naverLink:v.naverLink
+        })
+        handleOpenBottomSheet();
+    });
+
+    const customOverlay = new window.kakao.maps.CustomOverlay({
+      position: new kakao.maps.LatLng(v.latitude,v.longitude),
+      content: overlayDiv,
+      map : curmap,
+    });
+    customOverlay.setMap(curmap);
+    })
     
   }
   export default Marking
