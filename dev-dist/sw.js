a824bd3396f704908a6de3bfe0a8ab8bd61d13a9
zoo <anfdnfl@khu.ac.kr>
2025-02-18 21:43:22 +0900

    🐛 Fix: API URL 올바르게 수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.os0gp0j9ll"
+    "revision": "0.mp4emtq1kfg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
