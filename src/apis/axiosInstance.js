3a47094e93a284a7d7b007ee2d1194b94cfab753
Kiyeon Roh <rohkiyeon@naver.com>
2025-02-19 22:16:19 +0900

    ✨Feat: 리프레시 토큰 발급 추가

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -8,4 +8,32 @@ const TokenReq = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
 
+// 응답 인터셉터 추가
+TokenReq.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response && error.response.data.code === 4013) {
+      console.log("토큰 만료: 리프레시 토큰 요청 중...");
+      try {
+        // TokenReq 사용하여 리프레시 토큰 요청
+        const refreshResponse = await TokenReq.post("/auth/reissue");
+
+        if (refreshResponse.data.isSuccess) {
+          console.log("토큰 재발급 성공");
+          // 기존 요청을 재시도
+          return TokenReq(error.config);
+        } else {
+          console.log("토큰 재발급 실패");
+        }
+      } catch (refreshError) {
+        console.error("리프레시 토큰 요청 에러:", refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export { TokenReq };
