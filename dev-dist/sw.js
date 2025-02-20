5f751d9a6617dc714086ce04f3e96f81fe9c47fa
zoo <anfdnfl@khu.ac.kr>
2025-02-20 21:46:04 +0900

    ✨ Feat: 관리자 페이지 페이지네이션 추가

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d3d2cr98c28"
+    "revision": "0.vekkbkq3rdo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
