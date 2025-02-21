37abe9f0292f630e481d721df54ea7aee2aa0409
zoo <anfdnfl@khu.ac.kr>
2025-02-21 13:41:05 +0900

    merge

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vekkbkq3rdo"
+    "revision": "0.vatc9dsrrk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
